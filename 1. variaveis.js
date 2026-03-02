7eabe61efbf7bb2aa1a5c86efcaed26f679fb789
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-02 16:57:58 -0300

    salvar

---
+++ 1. variaveis.js
@@ -0,0 +1,15 @@
+// Variáveis.
+
+let nome = "Marta"
+let idade = 23
+let peso = 55.300
+let altura = 1.59
+
+// Testando variável.
+nome = "José"
+
+// Exibir no console.
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("Peso: ", peso)
+console.log("Altura: ", altura)
---
+++ 5. if-else-if.js
@@ -0,0 +1,13 @@
+// Estruturas condicionais.
+
+const idade = 15
+
+if (idade >= 18) {
+    console.log("Maioridade")
+} else if (idade >= 12) {
+    console.log("Adolescente")
+} else if (idade >= 2) {
+    console.log("Criança")
+} else {
+    console.log("Bebê")
+}
