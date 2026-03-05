22b170ab4a7b136c2a6c1f6a023775b4ade559e0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 16:55:46 -0300

    salvar

---
+++ 10. operadores - II.js
@@ -0,0 +1,13 @@
+// Operadores lógicos
+
+let condicao_a = 1 < 2 //True
+let condicao_b = 10 < 2 // False
+
+console.log(" - AND -")
+console.log(condicao_a && condicao_b)
+
+console.log("\n - NOT -")
+console.log(condicao_a && !condicao_b)
+
+console.log("\n - OR -")
+console.log(condicao_a || condicao_b)
---
+++ 11. funcao.js
@@ -0,0 +1,15 @@
+// Função comum no Javascript
+
+function somar(a, b) {
+    return a + b
+}
+
+function subtrair(a, b) {
+    return a - b
+}
+
+const soma = somar(2, 3)
+const subtracao = subtrair(2, 3)
+
+console.log("Soma: ", soma)
+console.log("Subtração: ", subtracao)
